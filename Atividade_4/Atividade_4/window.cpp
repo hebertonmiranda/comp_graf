03dc73f948cc7334d91c0da7d9009fec16a50584
Heberton Miranda <hmrs12@hotmail.com>
2022-12-11 10:14:33 -0300

    Upload activity 4 - Earth Visualization

---
+++ Atividade_4/Atividade_4/window.cpp
@@ -0,0 +1,221 @@
+#include "window.hpp"
+
+void Window::onEvent(SDL_Event const &event) {
+  glm::ivec2 mousePosition;
+  SDL_GetMouseState(&mousePosition.x, &mousePosition.y);
+
+  if (event.type == SDL_MOUSEMOTION) {
+    m_trackBallModel.mouseMove(mousePosition);
+    m_trackBallLight.mouseMove(mousePosition);
+  }
+  if (event.type == SDL_MOUSEBUTTONDOWN) {
+    if (event.button.button == SDL_BUTTON_LEFT) {
+      m_trackBallModel.mousePress(mousePosition);
+    }
+    if (event.button.button == SDL_BUTTON_RIGHT) {
+      m_trackBallLight.mousePress(mousePosition);
+    }
+  }
+  if (event.type == SDL_MOUSEBUTTONUP) {
+    if (event.button.button == SDL_BUTTON_LEFT) {
+      m_trackBallModel.mouseRelease(mousePosition);
+    }
+    if (event.button.button == SDL_BUTTON_RIGHT) {
+      m_trackBallLight.mouseRelease(mousePosition);
+    }
+  }
+  if (event.type == SDL_MOUSEWHEEL) {
+    m_zoom += (event.wheel.y > 0 ? -1.0f : 1.0f) / 5.0f;
+    m_zoom = glm::clamp(m_zoom, -1.5f, 1.0f);
+  }
+}
+
+void Window::onCreate() {
+  auto const assetsPath{abcg::Application::getAssetsPath()};
+
+  // window background color (black)
+  abcg::glClearColor(0, 0, 0, 1);
+  abcg::glEnable(GL_DEPTH_TEST);
+  abcg::glEnable(GL_CULL_FACE);
+
+  // Create programs
+  for (auto const &name : m_shaderNames) {
+    auto const path{assetsPath + "shaders/" + name};
+    auto const program{abcg::createOpenGLProgram(
+        {{.source = path + ".vert", .stage = abcg::ShaderStage::Vertex},
+         {.source = path + ".frag", .stage = abcg::ShaderStage::Fragment}})};
+    m_programs.push_back(program);
+  }
+
+  // Load default model
+  loadModel(assetsPath + "Earth_2K.obj");
+  m_mappingMode = 3; // "From mesh" option
+
+  // Initial trackball spin
+  m_trackBallModel.setAxis(glm::normalize(glm::vec3(1, 1, 1)));
+  m_trackBallModel.setVelocity(0.1f);
+}
+
+void Window::loadModel(std::string_view path) {
+  auto const assetsPath{abcg::Application::getAssetsPath()};
+
+  m_model.destroy();
+
+  m_model.loadDiffuseTexture(assetsPath + "maps/Diffuse_2K.png");
+  m_model.loadNormalTexture(assetsPath + "maps/Bump_2K.png");
+  m_model.loadNightTexture(assetsPath + "maps/Night_lights_2K.png");
+  m_model.loadObj(path);
+  m_model.setupVAO(m_programs.at(m_currentProgramIndex));
+  m_trianglesToDraw = m_model.getNumTriangles();
+
+  // Use material properties from the loaded model
+  m_Ka = m_model.getKa();
+  m_Kd = m_model.getKd();
+  m_Ks = m_model.getKs();
+  m_shininess = m_model.getShininess();
+}
+
+void Window::onPaint() {
+  abcg::glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  abcg::glViewport(0, 0, m_viewportSize.x, m_viewportSize.y);
+
+  // Use currently selected program
+  auto const program{m_programs.at(m_currentProgramIndex)};
+  abcg::glUseProgram(program);
+
+  // Get location of uniform variables
+  auto const viewMatrixLoc{abcg::glGetUniformLocation(program, "viewMatrix")};
+  auto const projMatrixLoc{abcg::glGetUniformLocation(program, "projMatrix")};
+  auto const modelMatrixLoc{abcg::glGetUniformLocation(program, "modelMatrix")};
+  auto const normalMatrixLoc{
+      abcg::glGetUniformLocation(program, "normalMatrix")};
+  auto const lightDirLoc{
+      abcg::glGetUniformLocation(program, "lightDirWorldSpace")};
+  auto const shininessLoc{abcg::glGetUniformLocation(program, "shininess")};
+  auto const IaLoc{abcg::glGetUniformLocation(program, "Ia")};
+  auto const IdLoc{abcg::glGetUniformLocation(program, "Id")};
+  auto const IsLoc{abcg::glGetUniformLocation(program, "Is")};
+  auto const KaLoc{abcg::glGetUniformLocation(program, "Ka")};
+  auto const KdLoc{abcg::glGetUniformLocation(program, "Kd")};
+  auto const KsLoc{abcg::glGetUniformLocation(program, "Ks")};
+  auto const diffuseTexLoc{abcg::glGetUniformLocation(program, "diffuseTex")};
+  auto const normalTexLoc{abcg::glGetUniformLocation(program, "normalTex")};
+  auto const nightTexLoc{abcg::glGetUniformLocation(program, "nightTex")};
+  auto const mappingModeLoc{abcg::glGetUniformLocation(program, "mappingMode")};
+
+  // Set uniform variables that have the same value for every model
+  abcg::glUniformMatrix4fv(viewMatrixLoc, 1, GL_FALSE, &m_viewMatrix[0][0]);
+  abcg::glUniformMatrix4fv(projMatrixLoc, 1, GL_FALSE, &m_projMatrix[0][0]);
+  abcg::glUniform1i(diffuseTexLoc, 0);
+  abcg::glUniform1i(normalTexLoc, 1);
+  abcg::glUniform1i(nightTexLoc, 2);
+  abcg::glUniform1i(mappingModeLoc, m_mappingMode);
+
+  auto const lightDirRotated{m_trackBallLight.getRotation() * m_lightDir};
+  abcg::glUniform4fv(lightDirLoc, 1, &lightDirRotated.x);
+  abcg::glUniform4fv(IaLoc, 1, &m_Ia.x);
+  abcg::glUniform4fv(IdLoc, 1, &m_Id.x);
+  abcg::glUniform4fv(IsLoc, 1, &m_Is.x);
+
+  // Set uniform variables for the current model
+  abcg::glUniformMatrix4fv(modelMatrixLoc, 1, GL_FALSE, &m_modelMatrix[0][0]);
+
+  // calculate the aspect of the window and set the projection
+  // view to perspective mode
+  auto const aspect{gsl::narrow<float>(m_viewportSize.x) /
+                    gsl::narrow<float>(m_viewportSize.y)};
+
+  m_projMatrix = glm::perspective(glm::radians(45.0f), aspect, 0.1f, 5.0f);
+
+  auto const modelViewMatrix{glm::mat3(m_viewMatrix * m_modelMatrix)};
+  auto const normalMatrix{glm::inverseTranspose(modelViewMatrix)};
+  abcg::glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, &normalMatrix[0][0]);
+
+  abcg::glUniform4fv(KaLoc, 1, &m_Ka.x);
+  abcg::glUniform4fv(KdLoc, 1, &m_Kd.x);
+  abcg::glUniform4fv(KsLoc, 1, &m_Ks.x);
+  abcg::glUniform1f(shininessLoc, m_shininess);
+
+  // call function to render the Earth according to the quantity
+  // of the triangles to draw
+  m_model.render(m_trianglesToDraw);
+
+  abcg::glUseProgram(0);
+}
+
+void Window::onUpdate() {
+  m_modelMatrix = m_trackBallModel.getRotation();
+
+  m_viewMatrix =
+      glm::lookAt(glm::vec3(0.0f, 0.0f, 2.0f + m_zoom),
+                  glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+}
+
+void Window::onPaintUI() {
+  abcg::OpenGLWindow::onPaintUI();
+
+  // Create main window widget
+  {
+    auto widgetSize{ImVec2(222, 87)};
+
+    if (!m_model.isUVMapped()) {
+      // Add extra space for static text
+      widgetSize.y += 26;
+    }
+
+    ImGui::SetNextWindowPos(ImVec2(m_viewportSize.x - widgetSize.x - 5, 5));
+    ImGui::SetNextWindowSize(widgetSize);
+    ImGui::Begin("Widget window", nullptr,
+                 ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDecoration);
+
+    // Slider will be stretched horizontally
+    ImGui::PushItemWidth(widgetSize.x - 16);
+    ImGui::SliderInt(" ", &m_trianglesToDraw, 0, m_model.getNumTriangles(),
+                     "%d triangles");
+    ImGui::PopItemWidth();
+
+    // Shader combo box
+    {
+      static std::size_t currentIndex{};
+
+      ImGui::PushItemWidth(120);
+      if (ImGui::BeginCombo("Shader", m_shaderNames.at(currentIndex))) {
+        for (auto const index : iter::range(m_shaderNames.size())) {
+          auto const isSelected{currentIndex == index};
+          if (ImGui::Selectable(m_shaderNames.at(index), isSelected))
+            currentIndex = index;
+          if (isSelected)
+            ImGui::SetItemDefaultFocus();
+        }
+        ImGui::EndCombo();
+      }
+      ImGui::PopItemWidth();
+
+      // Set up VAO if shader program has changed
+      if (gsl::narrow<int>(currentIndex) != m_currentProgramIndex) {
+        m_currentProgramIndex = gsl::narrow<int>(currentIndex);
+        m_model.setupVAO(m_programs.at(m_currentProgramIndex));
+      }
+    }
+
+    if (!m_model.isUVMapped()) {
+      ImGui::TextColored(ImVec4(1, 1, 0, 1), "Mesh has no UV coords.");
+    }
+
+    ImGui::End();
+  }
+}
+
+void Window::onResize(glm::ivec2 const &size) {
+  m_viewportSize = size;
+  m_trackBallModel.resizeViewport(size);
+  m_trackBallLight.resizeViewport(size);
+}
+
+void Window::onDestroy() {
+  m_model.destroy();
+  for (auto const &program : m_programs) {
+    abcg::glDeleteProgram(program);
+  }
+}
